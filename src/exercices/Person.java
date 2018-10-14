5091b4bd8ab4d4054e53002ed441c7b719963a7b
Steph WildCode <stephwildcode@gmail.com>
2018-10-14 17:45:04 +0200

    Done!

--- src/exercices/Person.java
+++ src/exercices/Person.java
@@ -2,41 +2,61 @@ package exercices;
 
 public class Person {
 
-    // TODO 1. Add the following private attributes :
-    // - name (String)
-    // - age (int)
-    // - wilder (boolean)
+	// TODO 1. Add the following private attributes :
+	// - name (String)
+	// - age (int)
+	// - wilder (boolean)
+	private String name;
+	private int age;
+	private boolean wilder;
 
+	// TODO 2. Add a Constructor with name and age arguments (in this order)
+	// This constructor must initialize name and age attributes
+	// Do not remove this empty constructor !
+	public Person() {
+	}
 
-    // TODO 2. Add a Constructor with name and age arguments (in this order)
-    // This constructor must initialize name and age attributes
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
 
+	// TODO 3. Add getter method for name attribute
+	public String getName() {
+		return name;
+	}
 
-    // TODO 3. Add getter method for name attribute
+	// TODO 4. Add setter method for name attribute
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	// TODO 5. Add getter method for age attribute
+	public int getAge() {
+		return age;
+	}
 
-    // TODO 4. Add setter method for name attribute
+	// TODO 6. Add setter method for age attribute
+	public void setAge(int age) {
+		this.age = age;
+	}
 
+	// TODO 7. Add getter method for wilder attribute
+	public boolean isWilder() {
+		return wilder;
+	}
 
-    // TODO 5. Add getter method for age attribute
+	// TODO 8. Add setter method for wilder attribute
+	public void setWilder(boolean wilder) {
+		this.wilder = wilder;
+	}
 
+	// TODO 9. Create a method 'whoAmI' that returns "My name is {name} and I'm
+	// {age}"
+	// where you replace {name} and {age} with attributes values
+	public String whoAmI() {
+		return "My name is " + name + " and I'm " + age;
+	}
 
-    // TODO 6. Add setter method for age attribute
-
-
-    // TODO 7. Add getter method for wilder attribute
-
-
-    // TODO 8. Add setter method for wilder attribute
-
-
-    // TODO 9. Create a method 'whoAmI' that returns "My name is {name} and I'm {age}"
-    // where you replace {name} and {age} with attributes values
-
-
-
-
-    // Do not remove this empty constructor !
-    public Person() {
-    }
 }
--- src/exercices/Primitives.java
+++ src/exercices/Primitives.java
@@ -2,19 +2,28 @@ package exercices;
 
 public class Primitives {
 
-    // TODO 1 : Change the method 'integer' to return the expected result : int 42
-    public static int integer() {
-        return 0;
-    }
+	// TODO 1 : Change the method 'integer' to return the expected result : int 42
+	public static int integer() {
+		return 42;
+	}
 
-    // TODO 2 : Create a method 'str', result expected : String "Hello"
+	// TODO 2 : Create a method 'str', result expected : String "Hello"
+	public String str() {
+		return "Hello";
+	}
 
+	// TODO 3 : Create a method 'real', result expected : double 3.14
+	public double real() {
+		return 3.14;
+	}
 
-    // TODO 3 : Create a method 'real', result expected : double 3.14
+	// TODO 4 : Create a method 'bool', result expected : boolean false
+	public boolean bool() {
+		return false;
+	}
 
-
-    // TODO 4 : Create a method 'bool', result expected : boolean false
-
-
-    // TODO 5 : Create a method 'nothing', no result expected
+	// TODO 5 : Create a method 'nothing', no result expected
+	public void nothing() {
+		
+	}
 }
